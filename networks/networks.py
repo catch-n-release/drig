6620f143b9387cbc2e1faae0bd7d60b1bf5903d3
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-03-27 23:14:34 +0530

    added custom dense network for fine-tuning

--- networks/networks.py
+++ networks/networks.py
@@ -59,3 +59,21 @@ class TinyVGG:
 
         except Exception as e:
             raise e
+
+
+class CustomDense:
+    @staticmethod
+    def compose(base_network, nodes, classes):
+        try:
+            custom_dense = base_network.output
+            ######
+            custom_dense = Flatten(name="flatten")(custom_dense)
+            ######
+            custom_dense = Dense(nodes, activation="relu")(custom_dense)
+            # custom_dense = Activation("relu")(custom_dense)
+            custom_dense = Dropout(0.5)(custom_dense)
+            custom_dense = Dense(classes, activation="softmax")(custom_dense)
+            # custom_dense = Activation("softmax")(custom_dense)
+            return custom_dense
+        except Exception as e:
+            raise e
