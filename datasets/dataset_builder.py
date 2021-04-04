16cf60453d2a3203b4e3959a27e797cbcd3e4d66
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-04-04 17:21:20 +0530

    updated HDF5 datum builder, tested. Works fine.

--- datasets/dataset_builder.py
+++ datasets/dataset_builder.py
@@ -1,7 +1,6 @@
 from enum import Enum
 import numpy as np
 import json
-import os
 import progressbar
 import cv2
 from imutils import paths
@@ -9,11 +8,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from drig.feature.feature import FeatureCondenser
 from drig.preprocessors.preprocessors import UniformAspectPreprocessor
+from drig.config import log, DogsVsCatsConfig
 
 
 class Hdf5DatumBuilder:
     def __init__(self, config: Enum):
         try:
+            log.info("-----INTIALIZING HDF5 DATASET BUILDER-----")
             self.config = config
             self.image_paths = list(paths.list_images(
                 config.IMAGES_PATH.value))
@@ -21,39 +22,107 @@ class Hdf5DatumBuilder:
                 config.LABEL_FROM_PATH(image_path)
                 for image_path in self.image_paths
             ]
+            self.red = list()
+            self.green = list()
+            self.blue = list()
+            self.preprocessing_height = 256
+            self.preprocessing_width = 256
+            self.preprocessing_depth = 3
+            self.aspect_preprocessor = UniformAspectPreprocessor(
+                self.preprocessing_height, self.preprocessing_width)
         except Exception as e:
             raise e
 
     def enconde_labels(self):
         try:
+            log.info("-----ENCODING LABELS-----")
             label_encoder = LabelEncoder()
-            return label_encoder.fit_transform(self.labels)
+            self.labels = label_encoder.fit_transform(self.labels)
         except Exception as e:
             raise e
 
     def train_val_test_split(self):
         try:
-            train_x_paths, test_x_paths, train_y_paths, test_y_paths = train_test_split(
+            log.info(
+                "-----SLITTING DATA INTO TRAINING VALIDATION & TESTING-----")
+            train_x_paths, test_x_paths, train_y_lables, test_y_labels = train_test_split(
                 self.image_paths,
                 self.labels,
                 test_size=self.config.NUM_TESTING_IMAGES.value,
                 random_state=42,
                 stratify=self.labels)
 
-            train_x_paths, val_x_paths, train_y_paths, val_y_paths = train_test_split(
+            train_x_paths, val_x_paths, train_y_lables, val_y_labels = train_test_split(
                 train_x_paths,
-                train_y_paths,
+                train_y_lables,
                 test_size=self.config.NUM_VALIDATION_IMAGES.value,
                 random_state=42,
-                stratify=self.labels)
+                stratify=train_y_lables)
 
             split_set = dict(
-                train=(train_x_paths, train_y_paths,
-                       self.config.TRAINING_DATUM_PATH.value),
-                validation=(val_x_paths, val_y_paths,
+                TRAINING=(train_x_paths, train_y_lables,
+                          self.config.TRAINING_DATUM_PATH.value),
+                VALIDATION=(val_x_paths, val_y_labels,
                             self.config.VALIDATION_DATUM_PATH.value),
-                test=(test_x_paths, test_y_paths,
-                      self.config.TESTING_DATUM_PATH.value))
+                TESTING=(test_x_paths, test_y_labels,
+                         self.config.TESTING_DATUM_PATH.value))
             return split_set
         except Exception as e:
             raise e
+
+    def compose(self):
+        try:
+
+            log.info("-----BUILDING HDF5 DATASET-----")
+            for set_type, (image_paths, labels,
+                           save_dir) in self.train_val_test_split().items():
+                condenser = FeatureCondenser(
+                    (len(image_paths), self.preprocessing_height,
+                     self.preprocessing_width, self.preprocessing_depth),
+                    save_dir)
+                widgets = [
+                    f"BUILDING {set_type} DATUM :",
+                    progressbar.Percentage(),
+                    " ",
+                    progressbar.Bar(marker="⎍"),
+                    " ",
+                    progressbar.SimpleProgress(),
+                    " ",
+                    progressbar.ETA(),
+                ]
+                prog_bar = progressbar.ProgressBar(maxval=len(image_paths),
+                                                   widgets=widgets).start()
+
+                for index, (image_path,
+                            label) in enumerate(zip(image_paths, labels)):
+
+                    image = cv2.imread(image_path)
+                    preprocessed_image = self.aspect_preprocessor.preprocess(
+                        image)
+                    if set_type == "TRAINING":
+                        b, g, r = cv2.mean(preprocessed_image)[:3]
+                        self.red.append(r)
+                        self.green.append(g)
+                        self.blue.append(b)
+                    condenser.commit([preprocessed_image], [label])
+                    prog_bar.update(index)
+
+            prog_bar.finish()
+            condenser.lock()
+            mean_rgb = dict(RED=np.mean(self.red),
+                            GREEN=np.mean(self.green),
+                            BLUE=np.mean(self.blue))
+            log.info("-----WRITING MEAN RGB JSON FILE-----")
+            json_file = open(self.config.MEAN_RGB.value, "w")
+            json_file.write(json.dumps(mean_rgb))
+            json_file.close()
+        except TypeError:
+            log.exception("---ERROR: LABELS NOT ENCODED----")
+        except Exception as e:
+            raise e
+
+
+# data_builder = Hdf5DatumBuilder(DogsVsCatsConfig)
+# data_builder.enconde_labels()
+# split_set = data_builder.train_val_test_split()
+# print(len(split_set.get("TRAINING")[0]))
