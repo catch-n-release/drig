2a6685e2bf9b18ef4f43a7b7922176ef20a75f32
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-04-30 17:52:04 +0530

    updated utils

--- utils.py
+++ utils.py
@@ -13,6 +13,8 @@ import plotly.graph_objects as graph
 from imutils import paths
 from PIL import ImageFont
 import os
+from drig.config import logging as log
+import json
 
 
 def display_image(image):
@@ -22,14 +24,26 @@ def display_image(image):
         raise e
 
 
-def plot_training_metrics(epochs,
-                          model_training_history,
-                          save_path=None,
-                          use_matplotlib=False,
-                          callback=False):
+def plot_training_metrics(model_training_history=None,
+                          epochs: int = None,
+                          json_path: str = None,
+                          save_path: str = None,
+                          use_matplotlib: bool = False,
+                          callback: bool = False,
+                          inline: bool = False):
     try:
 
-        if callback:
+        if json_path:
+            json_file = open(json_path, mode="r")
+            model_training_history = json.load(json_file)
+        elif epochs and model_training_history:
+            pass
+        else:
+            raise Exception(
+                "EITHER JSON PATH OR EPOCHS & MODEL TRAINING HISTORY SHOULD BE SUPPLIED"
+            )
+
+        if callback or json_path:
             y_training_loss, y_validation_loss, y_training_accuracy, y_validation_accuracy = model_training_history[
                 "loss"], model_training_history[
                     "val_loss"], model_training_history[
@@ -42,28 +56,37 @@ def plot_training_metrics(epochs,
                         "accuracy"], model_training_history.history[
                             "val_accuracy"]
         x_axis = np.arange(0, len(y_training_loss))
+        plot_values = x_axis, y_training_loss, y_validation_loss, y_training_accuracy, y_validation_accuracy
         if use_matplotlib:
-            if save_path:
+            if callback:
                 matplotlib.use("Agg")
-            plt.style.use("ggplot")
-            plt.figure(figsize=(8, 6), dpi=80)
-            plt.plot(x_axis, y_training_loss, label="Training Loss")
-            plt.plot(x_axis, y_validation_loss, label="Validation Loss")
-            plt.plot(x_axis, y_training_accuracy, label="Training Accuracy")
-            plt.plot(x_axis,
-                     y_validation_accuracy,
-                     label="Validation Accuracy")
-            plt.title("Training/Validation Loss & Accuracy")
-            plt.xlabel("EPOCH #")
-            plt.ylabel("LOSS/ACCURACY")
-            plt.legend()
-            if save_path:
-                plt.savefig(save_path)
-                return
-            plt.show()
+            # else:
+
+            matplotlib_plot(plot_values, callback, inline, save_path)
         else:
-            plot_values = x_axis, y_training_loss, y_validation_loss, y_training_accuracy, y_validation_accuracy
-            plot(plot_values, callback, save_path=save_path)
+            plot(plot_values, callback, inline, save_path=save_path)
+    except Exception as e:
+        raise e
+
+
+def matplotlib_plot(plot_values, callback, inline, save_path):
+    try:
+        x_axis, *loss_accuracy_value_list = plot_values
+        y_training_loss, y_validation_loss, y_training_accuracy, y_validation_accuracy = loss_accuracy_value_list
+        plt.style.use("ggplot")
+        plt.figure(figsize=(8, 6), dpi=80)
+        plt.plot(x_axis, y_training_loss, label="Training Loss")
+        plt.plot(x_axis, y_validation_loss, label="Validation Loss")
+        plt.plot(x_axis, y_training_accuracy, label="Training Accuracy")
+        plt.plot(x_axis, y_validation_accuracy, label="Validation Accuracy")
+        plt.title("Training/Validation Loss & Accuracy")
+        plt.xlabel("EPOCH #")
+        plt.ylabel("LOSS/ACCURACY")
+        plt.legend()
+        if save_path:
+            plt.savefig(save_path)
+        if not callback and inline:
+            plt.show()
     except Exception as e:
         raise e
 
@@ -157,13 +180,14 @@ def draw_faces(image, faces):
 def plot(
     plot_values,
     callback,
-    save_path=None,
+    inline,
+    save_path,
 ):
     try:
-        fig = graph.Figure()
+
         x_axis, *loss_accuracy_value_list = plot_values
         y_training_loss, y_validation_loss, y_training_accuracy, y_validation_accuracy = loss_accuracy_value_list
-
+        fig = graph.Figure()
         fig.add_trace(
             graph.Scatter(x=x_axis,
                           y=y_training_loss,
@@ -186,6 +210,8 @@ def plot(
                           name='Validation Accuracy'))
 
         fig.update_layout(autosize=False,
+                          plot_bgcolor="#d9d9d9",
+                          title="Training/Validation Loss & Accuracy",
                           width=1000,
                           height=600,
                           xaxis_title="EPOCH #",
@@ -194,7 +220,8 @@ def plot(
         if save_path:
             fig.write_image(save_path)
 
-        if not callback:
+        if not callback and inline:
             fig.show()
+
     except Exception as e:
         raise e
