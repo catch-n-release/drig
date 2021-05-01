630f90314c4332c0720702fe19b2b5412d122b75
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-01 20:28:48 +0530

    updated UniformAspect Processor

--- preprocessors/preprocessors.py
+++ preprocessors/preprocessors.py
@@ -23,7 +23,7 @@ class ShapePreprocessor:
 
 
 class UniformAspectPreprocessor():
-    def __init__(self, width, height, inter=cv2.INTER_AREA):
+    def __init__(self, height, width, inter=cv2.INTER_AREA):
         try:
             self.width = width
             self.height = height
