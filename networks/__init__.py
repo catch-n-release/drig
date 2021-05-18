2f76b5a3034a66d7dcc7657b8ecb5ac962a0f495
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-18 18:29:10 +0530

    updated networks init

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,1 +1,5 @@
-from drig.networks.networks import *
+from drig.networks.alex_net import AlexNet
+from drig.networks.custom_dense import CustomDense
+from drig.networks.nigh_google_net import NighGoogLeNet
+from drig.networks.tiny_google_net import TinyGoogLeNet
+from drig.networks.tiny_vgg_net import TinyVGGNet
