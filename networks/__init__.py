018d250a14ced4a6bbe66a7876d1fc537674547a
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-19 15:59:07 +0530

    added MLP

--- networks/__init__.py
+++ networks/__init__.py
@@ -3,3 +3,4 @@ from drig.networks.custom_dense import CustomDense
 from drig.networks.nigh_google_net import NighGoogLeNet
 from drig.networks.tiny_google_net import TinyGoogLeNet
 from drig.networks.tiny_vgg_net import TinyVGGNet
+from drig.networks.multi_layer_perceptron import MultiLayerPerceptron
