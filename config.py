9910d97003af6b9406f130acd610a1e98ee6624d
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-20 00:57:35 +0530

    added pre-defined image shapes in config

--- config.py
+++ config.py
@@ -123,3 +123,17 @@ class Trigger:
     RELU: str = "relu"
     SOFTMAX: str = "softmax"
     LINEAR: str = "linear"
+
+
+@dataclass(frozen=True)
+class ImageDim:
+    RGB_32x32: tuple = (32, 32, 3)
+    RGB_64x64: tuple = (64, 64, 3)
+    RGB_224x224: tuple = (224, 224, 3)
+    RGB_227x227: tuple = (227, 227, 3)
+    RGB_256x256: tuple = (256, 256, 3)
+    GRAY_32x32: tuple = (32, 32, 1)
+    GRAY_64x64: tuple = (64, 64, 1)
+    GRAY_224x224: tuple = (224, 224, 1)
+    GRAY_227x227: tuple = (227, 227, 1)
+    GRAY_256x256: tuple = (256, 256, 1)
