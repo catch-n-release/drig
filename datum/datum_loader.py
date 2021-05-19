415bf12a11f671492a46a195fc4dbe29d41117eb
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-19 15:58:58 +0530

    added csv data loader

--- datum/datum_loader.py
+++ datum/datum_loader.py
@@ -1,9 +1,12 @@
 import numpy as np
 import cv2
 import progressbar
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 
 
-class DatumLoader:
+class ImageDatumLoader:
     def __init__(self, preprocessors=None):
         try:
             self.preprocessors = preprocessors
@@ -43,7 +46,6 @@ class DatumLoader:
                         image = preprocessor.preprocess(image)
                 data.append(image)
                 labels.append(label)
-                # if verbose > 0 and i >= 0 and (i + 1) % verbose == 0:
 
                 prog_bar.update(i)
             prog_bar.finish()
@@ -60,5 +62,76 @@ class DatumLoader:
                 data = data.astype("float") / 255.0
             return data, labels
 
+        except Exception as e:
+            raise e
+
+
+class CSVDatumLoader:
+    def __init__(
+        self,
+        csv_path: str,
+        features: list = None,
+        splitter: str = " ",
+        skewed_filter: dict = None,
+    ):
+        try:
+            self.csv_path = csv_path
+            self.features = features
+            self.dataframe = pd.read_csv(
+                self.csv_path,
+                sep=splitter,
+                header=None,
+                names=self.features,
+            )
+            if skewed_filter:
+                for key, value in skewed_filter.items():
+                    unique_skewed_column_data = self.dataframe[
+                        key].value_counts().keys().tolist()
+                    total_count = self.dataframe[key].value_counts().tolist()
+                    for (data, count) in zip(unique_skewed_column_data,
+                                             total_count):
+                        if count < value:
+                            indices = self.dataframe[self.dataframe[key] ==
+                                                     data].index
+                            self.dataframe.drop(indices, inplace=True)
+
+        except Exception as e:
+            raise e
+
+    def load(
+        self,
+        train_split,
+        test_split,
+        continous_features: list = None,
+        categorical_features: list = None,
+    ):
+        try:
+            min_max_scaler = MinMaxScaler()
+            train_x_cont = min_max_scaler.fit_transform(
+                train_split[continous_features])
+            test_x_cont = min_max_scaler.transform(
+                test_split[continous_features])
+
+            train_x_cat = np.array(list())
+            test_x_cat = np.array(list())
+            for discrete_feature in categorical_features:
+                label_binarizer = LabelBinarizer().fit(
+                    self.dataframe[discrete_feature])
+                train_encoded_feature = label_binarizer.transform(
+                    train_split[discrete_feature])
+                test_encoded_feature = label_binarizer.transform(
+                    test_split[discrete_feature])
+                if not train_x_cat.any():
+                    train_x_cat = train_encoded_feature
+                    test_x_cat = test_encoded_feature
+                else:
+                    train_x_cat = np.hstack(
+                        [train_x_cat, train_encoded_feature])
+                    test_x_cat = np.hstack([test_x_cat, test_encoded_feature])
+
+            train_x = np.hstack([train_x_cat, train_x_cont])
+            test_x = np.hstack([test_x_cat, test_x_cont])
+
+            return train_x, test_x
         except Exception as e:
             raise e
