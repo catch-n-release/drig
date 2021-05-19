b2f599e425224ae2bc62b0c36e320859c5d46c5a
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-19 15:58:43 +0530

    updated dataset loader names

--- datum/__init__.py
+++ datum/__init__.py
@@ -1,3 +1,3 @@
 from drig.datum.batch_composer import HDF5BatchComposer
 from drig.datum.datum_composer import HDF5DatumComposer
-from drig.datum.datum_loader import DatumLoader
+from drig.datum.datum_loader import ImageDatumLoader, CSVDatumLoader
