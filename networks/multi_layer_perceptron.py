087bc8bceb84af3f79a1b73f2e1fb0fc938b2053
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-21 23:35:53 +0530

    update mlp

--- networks/multi_layer_perceptron.py
+++ networks/multi_layer_perceptron.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers.core import Activation
+from drig.config import Trigger
 
 
 class MultiLayerPerceptron:
@@ -11,11 +12,11 @@ class MultiLayerPerceptron:
             8,
             input_dim=nodes,
         ))
-        net.add(Activation("relu"))
+        net.add(Activation(Trigger.RELU))
         net.add(Dense(4))
-        net.add(Activation("relu"))
+        net.add(Activation(Trigger))
 
         if regressor:
             net.add(Dense(1))
-            net.add(Activation("linear"))
+            net.add(Activation(Trigger.LINEAR))
         return net
