da7e8faf58095f9e355b0d14cd1c3b551eca8cc5
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-28 05:34:53 +0530

    added image cast test

--- _tests/utils_test.py
+++ _tests/utils_test.py
@@ -1,7 +1,17 @@
 import pytest
-from drig.utils import display_image
+from drig.utils import display_image, image_cast
+import cv2
 
 
 def test_display_image_invalid_path():
     with pytest.raises(TypeError, match="INVALID IMAGE PATH"):
         display_image("random_image_path")
+
+
+def test_image_cast(random_image_path):
+    assert image_cast(random_image_path) == cv2.imread(random_image_path).shape
+
+
+def test_image_cast_invalid_path():
+    with pytest.raises(TypeError, match="INVALID IMAGE PATH"):
+        image_cast("random_image_path")
