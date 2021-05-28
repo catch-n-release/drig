03ae29948607ff590d442120e88dc56cf34dcfda
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-28 16:31:20 +0530

    added test image to array preprocessor

--- _tests/preprocessors/preprocessors_test.py
+++ _tests/preprocessors/preprocessors_test.py
@@ -1,5 +1,6 @@
-from drig.preprocessors import ShapePreprocessor, UniformAspectPreprocessor, NormalizationPreprocessor
+from drig.preprocessors import ShapePreprocessor, UniformAspectPreprocessor, NormalizationPreprocessor, ImageToArrayPreprocessor
 import cv2
+import numpy as np
 
 
 def test_shape_preprocessor(random_image):
@@ -38,3 +39,20 @@ def test_normalization_preprocessor(random_image):
     assert ((true_red == normalized_red_channel).any()
             and (true_green == normalized_green_channel).any()
             and (true_blue == normalized_blue_channel).any())
+
+
+def test_image_to_array_preprocessor(random_image):
+    height, width, depth = random_image.shape
+    image_array_preprocessor = ImageToArrayPreprocessor()
+    image_array = image_array_preprocessor.preprocess(random_image)
+    assert type(image_array) == np.ndarray
+    assert (height, width, depth) == image_array.shape
+
+
+def test_image_to_array_preprocessor_channels_first(random_image):
+    height, width, depth = random_image.shape
+    image_array_preprocessor = ImageToArrayPreprocessor(
+        data_format="channels_first")
+    image_array = image_array_preprocessor.preprocess(random_image)
+    assert type(image_array) == np.ndarray
+    assert (depth, height, width) == image_array.shape
