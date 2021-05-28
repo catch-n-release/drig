0f3d9c21f118e0b17efc55483a1ef83d7914bb7e
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-28 05:34:34 +0530

    added new fixture

--- _tests/conftest.py
+++ _tests/conftest.py
@@ -6,7 +6,12 @@ from drig.datum_config import AnimalsConfig as config
 
 
 @pytest.fixture
-def random_image():
+def random_image_path():
     random_image_path = np.random.choice(
         list(paths.list_images(config.DATASET_PATH)))
+    return random_image_path
+
+
+@pytest.fixture
+def random_image(random_image_path):
     return cv2.imread(random_image_path)
