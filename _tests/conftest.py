68e2af884b1485d808397ab47b73a9d8489de381
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-06-01 03:47:08 +0530

    updated test config

--- _tests/conftest.py
+++ _tests/conftest.py
@@ -3,8 +3,11 @@ import numpy as np
 from imutils import paths
 import cv2
 from drig.datum_config import AnimalsConfig as config
+from drig.config import ImageCast
 import os
 import shutil
+from keras.layers import Input
+from keras.layers.normalization import BatchNormalization
 
 
 @pytest.fixture
@@ -30,3 +33,15 @@ def test_dir():
 @pytest.fixture
 def invalid_image_path():
     return "invalid/image/path.jpg"
+
+
+@pytest.fixture
+def batch_norm_tensor():
+    influx = Input(shape=ImageCast.RGB_64x64)
+    axis = -1
+    batch_norm_tensor = BatchNormalization(
+        axis=axis,
+        epsilon=2e-5,
+        momentum=9e-1,
+    )(influx)
+    return batch_norm_tensor
