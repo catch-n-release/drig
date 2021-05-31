0b29878b5232cce8b2758c501935d63840ea0eaf
Suyash Srivastava <suyash.srivastava04@gmail.com>
2021-05-31 15:12:37 +0530

    updated utils tests

--- _tests/utils_test.py
+++ _tests/utils_test.py
@@ -1,8 +1,9 @@
 import pytest
 from drig._tests.conftest import config
 from drig.config import Error
-from drig.utils import display_image, image_cast, list_image_paths
+from drig.utils import display_image, image_cast, list_image_paths, image_class, random_image
 import cv2
+import numpy as np
 
 
 def test_display_image_invalid_path(invalid_image_path):
@@ -11,8 +12,9 @@ def test_display_image_invalid_path(invalid_image_path):
         display_image(invalid_image_path)
 
 
-def test_image_cast(random_image_path):
-    assert image_cast(random_image_path) == cv2.imread(random_image_path).shape
+def test_image_cast(random_testing_image_path):
+    assert image_cast(random_testing_image_path) == cv2.imread(
+        random_testing_image_path).shape
 
 
 def test_image_cast_invalid_path(invalid_image_path):
@@ -38,3 +40,45 @@ def test_list_image_paths_empty_dir(test_dir):
     with pytest.raises(Exception,
                        match=f"{Error.EMPTY_DATASET_ERROR} : {test_dir}"):
         list_image_paths(test_dir)
+
+
+def test_image_class(random_testing_image_path):
+    true_class_name = random_testing_image_path.split("/")[-2]
+    class_name = image_class(random_testing_image_path, -3)
+    assert true_class_name == class_name
+
+
+def test_random_image_from_dataset():
+
+    image, image_path, class_name = random_image(
+        dataset_path=config.DATASET_PATH,
+        class_index=-3,
+        return_image_path=True,
+    )
+    true_class_name = image_path.split("/")[-2]
+
+    assert (
+        type(image),
+        class_name,
+    ) == (
+        np.ndarray,
+        true_class_name,
+    )
+
+
+def test_random_image_from_image_paths():
+    all_image_paths = list_image_paths(config.DATASET_PATH)
+    image, image_path, class_name = random_image(
+        image_paths=all_image_paths,
+        class_index=-3,
+        return_image_path=True,
+    )
+    true_class_name = image_path.split("/")[-2]
+
+    assert (
+        type(image),
+        class_name,
+    ) == (
+        np.ndarray,
+        true_class_name,
+    )
